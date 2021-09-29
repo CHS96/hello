3f5a2a3bbcdd03dae7d900eb08717f5501a6a88f
CHS96 <sorjfkrh5078@gmail.com>
2021-09-29 18:31:17 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/hello/HelloApplication.kt
@@ -0,0 +1,12 @@
+package com.example.hello
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelloApplication
+
+fun main(args: Array<String>) {
+	runApplication<HelloApplication>(*args)
+}
